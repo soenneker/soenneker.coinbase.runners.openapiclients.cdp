013b287730b6a088df422830fa42162afd616007
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 10:45:32 -0500

    tunit

--- test/Soenneker.Coinbase.Runners.OpenApiClients.Cdp.Tests/CoinbaseCdpOpenApiClientRunnerTests.cs
+++ test/Soenneker.Coinbase.Runners.OpenApiClients.Cdp.Tests/CoinbaseCdpOpenApiClientRunnerTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Coinbase.Runners.OpenApiClients.Cdp.Tests;
 
-[Collection("Collection")]
-public sealed class CoinbaseCdpOpenApiClientRunnerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class CoinbaseCdpOpenApiClientRunnerTests : HostedUnitTest
 {
-    public CoinbaseCdpOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public CoinbaseCdpOpenApiClientRunnerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
