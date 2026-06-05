450bd83b128edfef1e8c3c2b4270d515c78b066b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:06 -0500

    xml commenting

--- src/Soenneker.Coinbase.Runners.OpenApiClients.Cdp/Constants.cs
+++ src/Soenneker.Coinbase.Runners.OpenApiClients.Cdp/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.Coinbase.Runners.OpenApiClients.Cdp;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Coinbase.OpenApiClients.Cdp";
 }
